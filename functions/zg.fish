2b293bf40642a5ae727dd685b9cd88ffa50d9157
mibo <mibo@mirb.de>
2018-08-31 18:29:49 +0200

    Added support for chars

--- functions/zg.fish
+++ functions/zg.fish
@@ -1,4 +1,5 @@
 # https://mvolkmann.github.io/fish-article/
+# https://github.com/jorgebucaran/fish-shell-cookbook/blob/master/README.md  
 function zg --description "Z helper"
     echo "Run with $argv..."
     
@@ -19,14 +20,78 @@ function zg --description "Z helper"
         # for se in (seq "$max")
         set -l rev_z_res zres[-1..1]
         for se in (seq $max 1)
-            echo "Found "selector[$se]":"$zres[$se]
+            echo "Found ["$selector[$se]"]: "$zres[$se]
         end
 
-        read --prompt "echo Select: " rval
-        set -l selVal $zres[$rval]
+        read -n 1 --prompt "echo Select: " rval
+        __rvalToNum $rval
+        #echo "TEST: $rvalNum"
+        set -l selVal $zres[$rvalNum]
         echo "Selected ($rval) $selVal"
         set -l selValSplit (echo $selVal| string split " ")
         echo "Go to $selValSplit[-1]"
         cd $selValSplit[-1]
+    end
+end
+
+function __rvalToNum
+    #echo $argv
+    switch "$argv"
+        case 0
+            set -g rvalNum 10
+        case a
+            set -g rvalNum 11
+        case b
+            set -g rvalNum 12
+        case c
+            set -g rvalNum 13
+        case d
+            set -g rvalNum 14
+        case e
+            set -g rvalNum 15
+        case f
+            set -g rvalNum 16
+        case g
+            set -g rvalNum 17
+        case h
+            set -g rvalNum 18
+        case i
+            set -g rvalNum 19
+        case j
+            set -g rvalNum 20
+        case k
+            set -g rvalNum 21
+        case l
+            set -g rvalNum 22
+        case m
+            set -g rvalNum 23
+        case n
+            set -g rvalNum 24
+        case o
+            set -g rvalNum 25
+        case p
+            set -g rvalNum 26
+        case q
+            set -g rvalNum 27
+        case r
+            set -g rvalNum 28
+        case s
+            set -g rvalNum 29
+        case t
+            set -g rvalNum 30
+        case u
+            set -g rvalNum 31
+        case v
+            set -g rvalNum 32
+        case w
+            set -g rvalNum 33
+        case x
+            set -g rvalNum 34
+        case y
+            set -g rvalNum 35
+        case z
+            set -g rvalNum 36
+        case \*
+            set -g rvalNum $argv
     end
 end
