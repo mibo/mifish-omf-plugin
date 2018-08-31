ace4350b5e7fdd34b6f02e5499446ab0b09051b8
mibo <mibo@mirb.de>
2018-08-31 17:58:46 +0200

    Fix when nothing was found

--- functions/zg.fish
+++ functions/zg.fish
@@ -9,21 +9,24 @@ function zg --description "Z helper"
     # end
 
     set -l max (count $zres)
+    if test $max -eq 0 
+        echo "Nothing found"
+    else 
+        set -l selector 1 2 3 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z
+        set -l selmax $selector[$max..1]
+        # echo $selector[1..3]
+        # echo "MAX $max"
+        # for se in (seq "$max")
+        set -l rev_z_res zres[-1..1]
+        for se in (seq $max 1)
+            echo "Found "selector[$se]":"$zres[$se]
+        end
 
-    set -l selector 1 2 3 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z
-    set -l selmax $selector[$max..1]
-    # echo $selector[1..3]
-    # echo "MAX $max"
-    # for se in (seq "$max")
-    set -l rev_z_res zres[-1..1]
-    for se in (seq $max 1)
-        echo "Found "selector[$se]":"$zres[$se]
+        read --prompt "echo Select: " rval
+        set -l selVal $zres[$rval]
+        echo "Selected ($rval) $selVal"
+        set -l selValSplit (echo $selVal| string split " ")
+        echo "Go to $selValSplit[-1]"
+        cd $selValSplit[-1]
     end
-
-    read --prompt "echo Select: " rval
-    set -l selVal $zres[$rval]
-    echo "Selected ($rval) $selVal"
-    set -l selValSplit (echo $selVal| string split " ")
-    echo "Go to $selValSplit[-1]"
-    cd $selValSplit[-1]
 end
