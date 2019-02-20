59418cce83e13e5d2cd69b5815db69be8a86ce70
mibo <mibo@mirb.de>
2019-02-20 07:36:49 +0100

    Simplified scan2inbox function

--- functions/scan2inbox.fish
+++ functions/scan2inbox.fish
@@ -1,14 +1,11 @@
 function scan2inbox -d 'Chmod for scans and move to DevonThink Inbox dir'
-    set -l l_scan_dir "~/Temp/Scanner"
-    set -l l_inbox_dir "~/Library/Application\ Support/DEVONthink\ Pro\ 2/Inbox"
+    set -l l_scan_dir ~/Temp/Scanner
+    set -l l_inbox_dir ~/Library/Application\ Support/DEVONthink\ Pro\ 2/Inbox
     set -l l_list (ls $l_scan_dir)
     if test -n "$l_list"
         echo "Found: $l_list"
         sudo chmod -R a+rw $l_scan_dir
-        set -l cmd "mv $l_scan_dir/* $l_inbox_dir"
-        #echo "$cmd"
-        eval $cmd
-        #mv $l_scan_dir/* $l_inbox_dir
+        mv $l_scan_dir/* $l_inbox_dir
     else
         echo "Nothing found in $l_scan_dir"
     end    
