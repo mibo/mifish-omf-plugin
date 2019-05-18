1055c0681b9b4e3542505a17ebdac3664d866a11
mibo <mibo@mirb.de>
2019-05-18 13:35:47 +0200

    Added some more open variants

--- functions/open.fish
+++ functions/open.fish
@@ -1,3 +1,18 @@
-function oidea -d 'open -a /Applications/IntelliJ\ IDEA.app pom.xml: Open pom.xml with IntelliJ'
-    open -a /Applications/IntelliJ\ IDEA.app pom.xml
+#
+# Collection of functions which use the `open` command from MacOS
+######
+function oa -d "open application"
+  # Package entry-point
+  # echo "Open default app for $argv..."
+  open $argv
+end
+
+function o -d "open default application"
+  echo "Open default with $argv..."
+  # Package entry-point
+  open $argv
+end
+
+function oidea -d 'Open given file with IntelliJ IDE'
+    open -a /Applications/IntelliJ\ IDEA.app $argv
 end
