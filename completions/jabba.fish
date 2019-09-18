d7de1fdc96887c755b50b1b73a87be68b684d5b5
mibo <mibo@mirb.de>
2019-09-18 22:21:01 +0200

    Added jabba use completion

---
+++ completions/jabba.fish
@@ -0,0 +1,9 @@
+# Check Fish documentation about completions:
+# http://fishshell.com/docs/current/commands.html#complete
+#
+# (Basic) Completion for Jabba
+#
+#########################
+set -l commands "install ls uninstall link unlink use current ls ls-remote deactivate alias unalias which"
+complete -c "jabba" -x -d "command" -n "not __fish_seen_subcommand_from $commands" -a "$commands"
+complete -c "jabba" -x -d "JVM to use" -n "__fish_seen_subcommand_from use" -a "(jabba ls)"
