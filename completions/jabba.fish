bb93f9d0277fc03548b979401c7b51bb433aabef
mibo <mibo@mirb.de>
2019-09-21 13:58:38 +0200

    Added completion for jabba install

--- completions/jabba.fish
+++ completions/jabba.fish
@@ -7,3 +7,4 @@
 set -l commands "install ls uninstall link unlink use current ls ls-remote deactivate alias unalias which"
 complete -c "jabba" -x -d "command" -n "not __fish_seen_subcommand_from $commands" -a "$commands"
 complete -c "jabba" -x -d "JVM to use" -n "__fish_seen_subcommand_from use" -a "(jabba ls)"
+complete -c "jabba" -x -d "to install" -n "__fish_seen_subcommand_from install" -a "(jabba ls-remote)"
