fba5dbe6e17c176123c8c9372142769548648d22
mibo <mibo@mirb.de>
2019-09-21 10:42:51 +0200

    Added abbr for git

--- init.fish
+++ init.fish
@@ -4,3 +4,8 @@
 # * $package       package name
 # * $path          package path
 # * $dependencies  package dependencies
+
+# Abbreviations - https://fishshell.com/docs/2.3/commands.html#abbr
+## Git
+abbr -a git_mb_rm "git branch -d (git branch --merged=master | grep -v master | cut -f 3 -d ' ')"
+abbr -a git_mb "git branch --merged=master | grep -v master | cut -f 3 -d ' '"
