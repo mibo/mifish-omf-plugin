1557b523e6dcb9f1ab5da794bdd266b55ffd5d60
mibo <mibo@mirb.de>
2019-09-21 13:59:54 +0200

    Changed l/ll

--- functions/l.fish
+++ functions/l.fish
@@ -1,3 +1,3 @@
-function l --description 'List contents of directory, including hidden files in directory using long human readable format'
-	ls -lAhG $argv
+function l --description 'List contents of directory using long human readable format'
+	ls -lhG $argv
 end
---
+++ functions/ll.fish
@@ -0,0 +1,3 @@
+function ll --description 'List contents of directory, including hidden items in directory using long human readable format'
+	ls -lAhG $argv
+end
