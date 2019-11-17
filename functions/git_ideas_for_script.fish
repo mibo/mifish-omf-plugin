9c56ab49a9e75c01366d1bc8a24bbc3277cee5ad
mibo <mibo@mirb.de>
2019-11-17 07:24:15 +0100

    Idea for git scripts

---
+++ functions/git_ideas_for_script.fish
@@ -0,0 +1,6 @@
+# https://dev.to/erykpiast/two-bash-scripts-i-cannot-live-without-when-working-with-git-44a1
+
+function git_ideas_for_script --description 'Idea for two git scripts based on bash. Check if worth to be ported'
+	echo "Idea for two git scripts based on bash. Check if worth to be ported"
+  echo "-> https://dev.to/erykpiast/two-bash-scripts-i-cannot-live-without-when-working-with-git-44a1"
+end
