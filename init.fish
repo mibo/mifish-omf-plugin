18fde898111c3cb4fc737a565ed02055f85a7dd8
mibo <mibo@mirb.de>
2020-02-13 07:55:05 +0100

    Added abbr for git remot prune

--- init.fish
+++ init.fish
@@ -7,7 +7,8 @@
 
 # Abbreviations - https://fishshell.com/docs/2.3/commands.html#abbr
 ## Git
+abbr -a git_mb_prune_remote "git remote prune origin"
+abbr -a git_mb_rm_dry "git branch --merged=master | grep -v master | cut -f 3 -d ' '"
 abbr -a git_mb_rm "git branch -d (git branch --merged=master | grep -v master | cut -f 3 -d ' ')"
-abbr -a git_mb "git branch --merged=master | grep -v master | cut -f 3 -d ' '"
 abbr --add -U git_mb_rm_gone_branch_dry "git branch -v | grep gone | cut -f 3 -d ' '"
 abbr --add -U git_mb_rm_gone_branch "git branch -D (git branch -v | grep gone | cut -f 3 -d ' ')"
