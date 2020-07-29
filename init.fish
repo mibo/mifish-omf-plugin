065130d816d76da672004ebffb60b62e5ddab6e8
mibo <mibo@mirb.de>
2020-07-29 06:37:35 +0200

    Added abbr for cf login

--- init.fish
+++ init.fish
@@ -14,5 +14,12 @@ abbr -a git_mb_rm_dry "git branch --merged=master | grep -v master | cut -f 3 -d
 abbr -a git_mb_rm "git branch -d (git branch --merged=master | grep -v master | cut -f 3 -d ' ')"
 abbr --add -U git_mb_rm_gone_branch_dry "git branch -v | grep gone | cut -f 3 -d ' '"
 abbr --add -U git_mb_rm_gone_branch "git branch -D (git branch -v | grep gone | cut -f 3 -d ' ')"
+# SAP CP CF
 abbr --add cf_canary_config "cp ~/.cf/config.canary.json ~/.cf/config.json"
 abbr --add cf_live_config "cp ~/.cf/config.live.json ~/.cf/config.json"
+#
+abbr -a cf_l_canary_dev "cf l -u d046871 -o ph-fosstars -s fosstars-dev"
+abbr -a cf_l_canary_dev_mibo "cf l -u d046871 -o ph-fosstars -s fosstars-dev-mibo"
+abbr -a cf_l_canary_prod "cf l -u d046871 -o ph-fosstars -s fosstars"
+abbr -a cf_l_canary_live_staging "cf l -u d046871 -o ph-fosstars-staging -s fosstars-staging"
+abbr -a cf_l_canary_live_prod "cf l -u d046871 -o ph-fosstars-staging -s fosstars-staging"
