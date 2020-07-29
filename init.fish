af2b0e5b8b0f918e22b79d21da00911368b8593a
mibo <mibo@mirb.de>
2020-07-29 06:23:22 +0200

    Added abbr for cf config copy

--- init.fish
+++ init.fish
@@ -14,3 +14,5 @@ abbr -a git_mb_rm_dry "git branch --merged=master | grep -v master | cut -f 3 -d
 abbr -a git_mb_rm "git branch -d (git branch --merged=master | grep -v master | cut -f 3 -d ' ')"
 abbr --add -U git_mb_rm_gone_branch_dry "git branch -v | grep gone | cut -f 3 -d ' '"
 abbr --add -U git_mb_rm_gone_branch "git branch -D (git branch -v | grep gone | cut -f 3 -d ' ')"
+abbr --add cf_canary_config "cp ~/.cf/config.canary.json ~/.cf/config.json"
+abbr --add cf_live_config "cp ~/.cf/config.live.json ~/.cf/config.json"
