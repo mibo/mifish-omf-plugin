c78e238e6005056e7c5d545f6a11c887965a7840
mibo <mibo@mirb.de>
2023-04-05 10:07:27 +0200

    Removed abbr in universal variable

--- init.fish
+++ init.fish
@@ -12,11 +12,11 @@ set -x LANG en_US.UTF-8
 abbr -a git_mb_prune_remote "git remote prune origin"
 abbr -a git_mb_rm_dry "git branch --merged=master | grep -v master | cut -f 3 -d ' '"
 abbr -a git_mb_rm "git branch -d (git branch --merged=master | grep -v master | cut -f 3 -d ' ')"
-abbr --add -U git_mb_rm_gone_branch_dry "git branch -v | grep gone | cut -f 3 -d ' '"
-abbr --add -U git_mb_rm_gone_branch "git branch -D (git branch -v | grep gone | cut -f 3 -d ' ')"
+abbr -a git_mb_rm_gone_branch_dry "git branch -v | grep gone | cut -f 3 -d ' '"
+abbr -a git_mb_rm_gone_branch "git branch -D (git branch -v | grep gone | cut -f 3 -d ' ')"
 # SAP CP CF
-abbr --add cf_canary_config "cp ~/.cf/config.canary.json ~/.cf/config.json"
-abbr --add cf_live_config "cp ~/.cf/config.live.json ~/.cf/config.json"
+abbr -a cf_canary_config "cp ~/.cf/config.canary.json ~/.cf/config.json"
+abbr -a cf_live_config "cp ~/.cf/config.live.json ~/.cf/config.json"
 #
 abbr -a cf_l_canary_dev "cf l -u d046871 -o ph-fosstars -s fosstars-dev"
 abbr -a cf_l_canary_dev_mibo "cf l -u d046871 -o ph-fosstars -s fosstars-dev-mibo"
